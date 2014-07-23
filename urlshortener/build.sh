76f4524ca4c36a7d74df003d6a8f4b6c79ab1cb9
Olivier Chafik <ochafik@google.com>
2014-07-23 09:37:18 -0700

    Use new mixin codegen

--- urlshortener/build.sh
+++ urlshortener/build.sh
@@ -7,18 +7,21 @@ PROJECT_ROOT=$PWD
 PROTOC_PLUGIN_ROOT=$PWD/../dart-protoc-plugin
 STREAMY_ROOT=$PWD/..
 
-PROTO_OUT=$PROJECT_ROOT/lib/proto
+PROTO_OUT=$PROJECT_ROOT/lib/mixin_autogen
 DISCOVERY_OUT=$PROJECT_ROOT/lib/discovery
 
 mkdir -p $PROTO_OUT $DISCOVERY_OUT
 
 cd $PROTOC_PLUGIN_ROOT
+pub get
 make build-plugin
 
 cd $PROJECT_ROOT
+pub get
 protoc --plugin=$PROTOC_PLUGIN_ROOT/out/protoc-gen-dart urlshortener.proto --dart_out=$PROTO_OUT
 
 cd $STREAMY_ROOT
+pub get
 dart bin/apigen.dart --discovery-file=$PROJECT_ROOT/urlshortener.json --client-file-name=urlshortener --output-dir=$DISCOVERY_OUT
 mv $DISCOVERY_OUT/lib/*.dart $DISCOVERY_OUT
 rm $DISCOVERY_OUT/pubspec.yaml
